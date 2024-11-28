f92790a2dd02de307ac77bcc7d872853e8703762
Gospel <83763085+NwaAmadi@users.noreply.github.com>
2024-11-28 11:27:16 +0100

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -159,4 +159,5 @@ CORS(app)
 
 if __name__ == '__main__':
     # Use Waitress to serve the application
-    serve(app, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
